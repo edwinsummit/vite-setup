dca60d9a0b3460212f53f092193a3b5c46d6ae12
Edwin Otten <edwin@summit.nl>
2022-03-16 16:05:50 +0100

    Add @ imports according to step#3 in guide https://vueschool.io/articles/vuejs-tutorials/how-to-migrate-from-vue-cli-to-vite/

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import eslintPlugin from "vite-plugin-eslint";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
