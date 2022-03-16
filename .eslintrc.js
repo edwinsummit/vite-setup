02f8e4279368e08952afed2d475383347b63f410
Edwin Otten <edwin@summit.nl>
2022-03-16 14:38:09 +0100

    Add prettier and autolint to vite dev experience according to guide in combination with EduDashboard linting config https://vueschool.io/articles/vuejs-tutorials/eslint-and-prettier-with-vite-and-vue-js-3/

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,24 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    "vue/setup-compiler-macros": true,
   },
   extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
+    "no-console": process.env.NODE_ENV === "development" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "development" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
 };
